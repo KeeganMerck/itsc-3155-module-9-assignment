954749cb824ac0391f719a1e04873b276c8204ea
Will <williamehemric@gmail.com>
2022-10-24 21:12:40 -0400

    create movie e2e and unit tests

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,14 @@
-# TODO: Feature 2
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+movie_repository = get_movie_repository()
+
+def test_create_movie():
+
+    movie_repository.create_movie('Star Wars IV', 'George Lucas', 5)
+
+    movieIV = Movie('Star Wars IV', 'George Lucas', 5)
+
+    assert movie_repository.get_all_movies()[0].title == movieIV.title
+
+    movie_repository._db.clear()
