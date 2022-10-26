8bb98da4cbec735ca37138eea77f2b336f111fee
Amine Aboualkacim <aaboualk@uncc.edu>
2022-10-25 23:22:48 -0400

    Implement test function for both e2e and Unit

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,15 @@
-# TODO: Feature 3
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+movie_repository = get_movie_repository()
+
+def test_get_movie_by_title():
+    movie_repository.create_movie('Minions', 'Gru', 5)
+    movie_repository.create_movie('Star Wars V', 'George Lucas', 3)
+    movie_repository.create_movie('Harry Potter', 'J.K Rowling', 4)
+
+    assert type(movie_repository.get_movie_by_title('Star Wars V')) == Movie
+    assert type(movie_repository.get_movie_by_title('Minions')) == Movie
+    assert movie_repository.get_movie_by_title('Transformers') == None
+
+    movie_repository._db.clear()
